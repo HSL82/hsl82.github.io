6e29a8aa81230a85fe7a3a15c80b5aed07caa364
Andrew Ho <hosl808@gmail.com>
2020-12-16 20:50:55 +0800

    re-create marker found play lost pause function

--- js/script.js
+++ js/script.js
@@ -1,69 +1,30 @@
-var audio = new Audio("bgm.mp3");
 const player = document.querySelector("#animationPlayer");
 const marker = document.querySelector("#animated-marker");
 const cover = document.querySelector("#cover");
 const loading = document.querySelector("#loading");
-const btnStart = document.querySelector("#btnStart");
-const btnRefresh = document.querySelector("#btnRefresh");
-
 var clicked = false;
-var markerVisible = false;
 var firstVisible = false;
 var animationStarted = false;
 //init
 cover.style.display = "block";
 
-function soundhandler() {
-  setInterval(() => {
-    if (clicked && markerVisible && animationStarted) {
-      audio.play();
-    } else {
-      audio.pause();
-    }
-  }, 700);
-}
-
-btnStart.addEventListener(
-  "click",
-  function () {
-    cover.style.display = "none";
-    audio.play();
-    audio.pause();
-    clicked = true;
-  },
-  {once: true}
-);
-
-btnRefresh.addEventListener(
-  "click",
-  function () {
-    location.reload();
-  },
-  {once: true}
-);
-
-player.addEventListener(
-  "animation-finished",
-  function () {
-    player.setAttribute("animation-mixer", "timeScale: 0");
-  },
-  {once: true}
-);
+player.addEventListener("animation-finished", function () {
+  console.log("done");
+  player.setAttribute("animation-mixer", "timeScale: 0");
+});
 
 marker.addEventListener("markerFound", function () {
-  console.log("test5");
-  markerVisible = true;
+  console.log("marker found");
   player.setAttribute(
     "animation-mixer",
-    "clip: Take 001; repetitions: 1; timeScale: 1"
+    "clip: All Animations; repetitions: 1; timeScale: 1"
   );
+
   if (!firstVisible) {
-    loading.style.display = "block";
+    loading.style.display = "flex";
 
     setTimeout(() => {
       player.setAttribute("visible", "true");
-      player.setAttribute("shadow", "cast: true; receive: true");
-
       firstVisible = true;
       animationStarted = true;
     }, 700);
@@ -75,11 +36,7 @@ marker.addEventListener("markerFound", function () {
 });
 
 marker.addEventListener("markerLost", function () {
-  // if (!firstVisible) {
-  console.log("test3");
+  console.log("marker lost");
   if (animationStarted == true)
     player.setAttribute("animation-mixer", "timeScale: 0");
-  markerVisible = false;
-
-  // }
 });
